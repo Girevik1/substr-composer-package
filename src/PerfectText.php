bd74df9dadb3c8beade8104bdfdcc9a17fce2707
Artur <se6@rep.earth>
2023-05-17 13:06:24 +0300

    ADD php doc and fix text readme

--- src/PerfectText.php
+++ src/PerfectText.php
@@ -6,6 +6,10 @@ namespace Girevik1\SubstrComposerPackage;
 
 class PerfectText
 {
+    /**
+     * Получаем текст нужной длинны, с заглавной буквы и с добавлением в конце - переданных символов.
+     * 
+     */
     public function getNeedFormatText(string $text, int $length, string $end, string $encoding = 'UTF-8'): string
     {
         return mb_substr($text, 0, $length, $encoding) . $end;
